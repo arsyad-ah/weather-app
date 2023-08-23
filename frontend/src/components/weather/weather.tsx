7e5905e8051655e1ef7237be766295968dead1a6
arsyad <38887752+Arsyad-ah@users.noreply.github.com>
2023-08-23 20:38:43 +0800

    add weather images

--- frontend/src/components/weather/weather.tsx
+++ frontend/src/components/weather/weather.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import {Paragraph} from '../../shared/style'
 import { WeatherDto } from "../../dto";
 import { DatetimeFormatter } from "../../shared/utils";
+import WeatherImage from "./weather-mapping";
+
+const EMPTY_FORECAST = 'No forecast available'
 
 interface WeatherInfoProps {
   weather: WeatherDto | null;
@@ -11,13 +14,16 @@ const WeatherInfo: React.FC<WeatherInfoProps> = ({ weather }) => {
 
   const formatTimestamp = DatetimeFormatter(weather?.timestamp || null)
 
+  const forecast = weather?.forecast || EMPTY_FORECAST;
+
   return (
     <div>
       <h2>Weather Information</h2>
       {(weather) ? (
         <div>
-          <h4>{`Forecast: ${weather?.forecast}`}</h4>
+          <h4>{`Forecast: ${forecast}`}</h4>
           <Paragraph>{`Location: ${weather?.location}`}</Paragraph>
+          <WeatherImage weatherCondition={forecast}></WeatherImage>
           <Paragraph>{`Correct as of: ${formatTimestamp}`}</Paragraph>
         </div>
       ) : <Paragraph>Please select location and date & time</Paragraph>}
